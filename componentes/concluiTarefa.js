68425846e3de286e2f5844a5473c034a6330b304
Matheus <matheusa026@gmail.com>
2025-08-22 20:55:12 -0300

    subindo alterações de componentes

---
+++ componentes/concluiTarefa.js
@@ -0,0 +1,25 @@
+const BotaoConclui = () => {
+    const botaoConclui = document.createElement('button');
+    
+    botaoConclui.classList.add('check-button');
+    botaoConclui.innerText = 'concluir'
+
+
+    botaoConclui.addEventListener('click', concluirTarefa);
+
+
+    return botaoConclui;
+
+}
+
+
+const concluirTarefa = (evento) => {
+    const eventoAcionado = evento.target
+
+    const tarefaCompleta = eventoAcionado.parentElement
+
+    tarefaCompleta.classList.toggle('done');
+}
+
+
+export default BotaoConclui;
